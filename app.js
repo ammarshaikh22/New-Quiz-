f0087f04bd9bd0e370251df394125a1fc71d90e6
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-04-22 14:31:03 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const main = async () => {
+    const question = [
+        {
+            message: "Why do we use TypeScript?",
+            name: "ques1",
+            type: "list",
+            choices: [
+                "for user interface",
+                "to make interactive web page",
+                "for strong typing",
+            ],
+        },
+        {
+            message: "What is HTML?",
+            name: "ques2",
+            type: "list",
+            choices: [
+                "hyper text mega link",
+                "hyper text markup language",
+                "hyper text mini language",
+            ],
+        },
+    ];
+    const show_ans = await inquirer.prompt(question);
+    let correct_ans = {
+        ques1: "for strong typing",
+        ques2: "hyper text markup language"
+    };
+    let score = 0;
+    for (let ans of Object.keys(show_ans)) {
+        const user_ans = show_ans[ans];
+        const ques_ans = correct_ans[ans];
+        if (user_ans === ques_ans) {
+            console.log("correct");
+            score++;
+        }
+        else {
+            console.log('incorrect!');
+        }
+    }
+    console.log(`The result is ${score}/${Object.keys(question).length}`);
+};
+main();
---
+++ app.ts
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+interface ques {
+    [key: string]: any
+}
+
+const main = async () => {
+    const question: ques = [
+        {
+            message: "Why do we use TypeScript?",
+            name: "ques1",
+            type: "list",
+            choices: [
+                "for user interface",
+                "to make interactive web page",
+                "for strong typing",
+            ],
+        },
+        {
+            message: "What is HTML?",
+            name: "ques2",
+            type: "list",
+            choices: [
+                "hyper text mega link",
+                "hyper text markup language",
+                "hyper text mini language",
+            ],
+        },
+    ];
+    const show_ans = await inquirer.prompt(question)
+
+    let correct_ans: any = {
+        ques1: "for strong typing",
+        ques2: "hyper text markup language"
+    }
+    let score: number = 0
+
+    for (let ans of Object.keys(show_ans)) {
+        const user_ans = show_ans[ans];
+        const ques_ans = correct_ans[ans];
+        if (user_ans === ques_ans) {
+            console.log("correct")
+            score++
+        } else {
+            console.log('incorrect!')
+        }
+    }
+
+    console.log(`The result is ${score}/${Object.keys(question).length}`)
+};
+
+main()
